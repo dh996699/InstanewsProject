0d275a404fbd21dc40773520cb7ebebb1816a1eb
David Huynh <dh99.66.99@gmail.com>
2019-07-24 14:11:56 -0700

    add get ajax from NYT

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-console.log ("hello World");
+$(function () {
+
+    function loadAjax(selected) {
+        console.log('load ajax');
+        $.ajax({
+            method: 'get',
+            url: 'https://api.nytimes.com/svc/topstories/v2/'+selected+'.json?api-key=loF5B7QAARl6Au2xTNto2IyPL2A7bmUt'
+
+        })
+            .done(function (data) {
+                console.log(data);
+                const results = data.result;
+                // // $each.()
+                // // append an article template`<article><p>${abstract}</p></article>
+
+            })
+            .fail(function (err) {
+                console.log(err);
+            })
+
+            .always(function () {
+                // hide loading gif
+            });
+    }// end of loadAjax
+
+
+    $('#article-select').on('change', function () {
+        console.log('change');
+        const select = $(this).val();
+        if (select !== '') {
+            loadAjax(select);
+        }
+    });
+
+
+
+ }); //End of document ready.
