54d0e6ae490e8b66c9cc57ba431285a49f3507ae
David Huynh <dh99.66.99@gmail.com>
2019-07-29 21:48:02 -0700

    pull image, link and abstract

--- js/main.js
+++ js/main.js
@@ -1,62 +1,50 @@
 $(function () {
- 
-    $('.loading').hide();
-
-    function loadAjax(selected) {
-        // console.log('loadAjax');
-        $.ajax({
-            method: 'get',
-            url: 'https://api.nytimes.com/svc/topstories/v2/'+selected+'.json?api-key=loF5B7QAARl6Au2xTNto2IyPL2A7bmUt'
-
-        })
-
-            .done(function (data) {
-                console.log(data);
-                $('.article-link').html('');
-                const results = data.results;
-                // console.log(results);
-        $.each(results, function(key, value) {
-            // console.log(value.title);
-            const title = value.title;
-            const abstract = value.abstract;
-
-        $('.article-link').append(`<div>${title, abstract}</div>`);
-        });
-                // // $each.()
-                // // append an article template`<article><p>${abstract}</p></article>
-                // $repoList.html('');
-                // $.each(data, function(key, value) {
-                //   // console.log(value.name);
-                //   const name = value.name;
-                //   $repoList.append(`<li>${name}</li>`);
-                // });
-            })
-            .fail(function (err) {
-                // console.log(err);
-            })
-
-            .always(function () {
-                // hide loading gif
-            });
-    }// end of loadAjax
-
-
+    $('#article-select').before($('.loading').hide());
     $('#article-select').on('change', function () {
-        // console.log('change');
         const select = $(this).val();
-        if (select !== '') {
-            $('#article-select').before($('.loading').show());
-            loadAjax(select);
-        }
+
+        $('#article-select').before($('.loading').show());
+
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.nytimes.com/svc/topstories/v2/' + select + '.json?api-key=loF5B7QAARl6Au2xTNto2IyPL2A7bmUt'
+
+        }).done(function (data) {
+            console.log(data);
+            const filteredData = data.results.filter(function (item) {
+                return item.multimedia[4] !== undefined;
+                // if (item.multimedia[4] !== undefined) {
+                //     return true
+                // } else { return false }
+            }).slice(0, 12);
+
+            // const results = image.slice(0, 12)
+
+            $.each(filteredData, function (key, value) {
+                console.log(value);
+                const abstract = value.abstract;
+                const pic = value.multimedia[4].url;
+                const link = value.short_url;
+
+                const htmlTemplate = `
+                    <a href="${link}" class="article" style="background: url(${pic}) center/cover;">
+                        <p>${abstract}</p>
+                    </a>
+                `;
+
+                $('.articles').append(htmlTemplate);
+                // $('.article-link').append(`<div>${pic}</div>`);
+
+
+            })
+
+        }).fail(function () {
+            console.log("hello");
+        }).always(function () {
+            // end of loadAjax
+            // hide loading gif
+            $('.loading').hide();
+        });
     });
 
-
-
-    // $repoList.html('');
-    //   $.each(data, function(key, value) {
-    //     // console.log(value.name);
-    //     const name = value.name;
-    //     $repoList.append(`<li>${name}</li>`);
-    //   });
-
- }); //End of document ready.
+}); //End of document ready.
