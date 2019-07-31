436cba31d1e319c3e4e83e5e30bcb366f5318b8b
David Huynh <dh99.66.99@gmail.com>
2019-07-30 23:23:21 -0700

    start @media 600

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ $(function () {
     $('#article-select').before($('.loading').hide());
     $('#article-select').on('change', function () {
         const select = $(this).val();
+
         $('.articles').html('');
+        $('.logo-header').addClass('is-active');
         $('#article-select').before($('.loading').show());
 
-        $('.logo-header').toggleClass('is-active');
 
         $.ajax({
             method: 'GET',
             url: 'https://api.nytimes.com/svc/topstories/v2/' + select + '.json?api-key=loF5B7QAARl6Au2xTNto2IyPL2A7bmUt'
 
         }).done(function (data) {
-            console.log(data);
             const filteredData = data.results.filter(function (item) {
                 return item.multimedia[4] !== undefined;
                 // if (item.multimedia[4] !== undefined) {
@@ -45,10 +45,12 @@ $(function () {
             // end of loadAjax
             // hide loading gif
             $('.loading').hide();
-            // $('.logo').css({
-            //     'width': '150px',
-            //     'height': '150px'
-            // });
+
+            // if ($('select') == section) {
+            //     console.log(select);
+            //     $('.logo-header').removeClass('is-active');
+            // })
+
         });
     });
 
